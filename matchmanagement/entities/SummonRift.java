4379abfbaae4b8228618595d9e05294ca9c20193
stronglight2000 <stronglight2000@gmail.com>
2024-08-29 11:17:44 +0700

    Bài tập tiktok

--- matchmanagement/entities/SummonRift.java
+++ matchmanagement/entities/SummonRift.java
@@ -3,21 +3,21 @@ package matchmanagement.entities;
 import java.util.ArrayList;
 
 public class SummonRift {
-     private ArrayList<Team> team1;
-     private ArrayList<Team> team2;
+     private ArrayList<Team> SKT;
+     private ArrayList<Team> G2;
      private String startTime;
 
-    public SummonRift(ArrayList<Team> team1, ArrayList<Team> team2, String startTime) {
-        this.team1 = team1;
-        this.team2 = team2;
+    public SummonRift(ArrayList<Team> SKT, ArrayList<Team> G2, String startTime) {
+        this.SKT = SKT;
+        this.G2 = G2;
         this.startTime = startTime;
     }
 
     @Override
     public String toString() {
         return "SummonRift{" +
-                "team1=" + team1 +
-                ", team2=" + team2 +
+                "team1=" + SKT +
+                ", team2=" + G2 +
                 ", startTime='" + startTime + '\'' +
                 '}';
     }
